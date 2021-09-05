f6d8af89a2c186c3499000a0a02f1fd4ab8808c6
nauman3s <naumanshakir3s@gmail.com>
2021-09-05 22:26:02 +0500

    updated installer

--- installer.sh
+++ installer.sh
@@ -1,1 +1,29 @@
+#-e option instructs bash to immediately exit if any command [1] has a non-zero exit status
+# We do not want users to end up with a partially working install, so we exit the script
+# instead of continuing the installation with something broken
+set -e
+
 echo "IOT Management System Client Installer"
+
+######## VARIABLES #########
+
+# Location for final installation log storage
+installLogLoc=/etc/pihole/install.log
+
+
+echo `Welcome user $USER`
+
+show_ascii_berry() {
+  echo -e "
+       _________________________   _______    _______         _______ 
+\__   __(  ___  \__   __/  (       )  (  ____ |\     /(  ____ \
+   ) (  | (   ) |  ) (     | () () |  | (    \( \   / | (    \/
+   | |  | |   | |  | |     | || || |  | (_____ \ (_) /| (_____ 
+   | |  | |   | |  | |     | |(_)| |  (_____  ) \   / (_____  )
+   | |  | |   | |  | |     | |   | |        ) |  ) (        ) |
+___) (__| (___) |  | |     | )   ( |  /\____) |  | |  /\____) |
+\_______(_______)  )_(     |/     \|  \_______)  \_/  \_______)
+                                                               
+"
+}
+show_ascii_berry
