1cb18c180ec7111ff654de1fb4fb142f4316f5c6
Nauman3S <naumanshakir3s@gmail.com>
2022-09-04 10:51:43 +0200

    updated files

--- RPiClient-rs/updateFW.sh
+++ RPiClient-rs/updateFW.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-sshpass -p "raspberry" scp -r * pi@raspberrypi.local:/home/pi/RPiClient-rs/
+#B827EB407E5A
+#sshpass -p "raspberry" scp -r * pi@raspberrypi.local:/home/pi/RPiClient-rs/
+sshpass -p "PER$P3CTIV3TEST" scp -r RPiClient-rs pi@192.168.178.71:/home/pi/RPiClient-rs/
