f8cd26b6c2fb75067668dd8cc06a124a34964152
Nauman3S <naumanshakir3s@gmail.com>
2022-09-20 16:06:42 +0200

    updated client

--- RPiClient-rs/src/main.rs
+++ RPiClient-rs/src/main.rs
@@ -148,8 +148,7 @@ async fn main() {
             format!("{}{}", "iotm-sys/device/firmware/url/", get_MAC()),
             QOS_1,
         );
-        cli.subscribe("iotm-sys/device/client/url/all", QOS_1)
-            .await;
+        cli.subscribe("iotm-sys/device/client/url/all", QOS_1).await;
         cli.subscribe(
             format!("{}{}", "iotm-sys/device/client/url/", get_MAC()),
             QOS_1,
@@ -254,19 +253,19 @@ async fn main() {
                                 println!("link={} flname={}", data_link, flname);
                                 download_file(&data_link.to_string(), &flname.to_string()).await;
 
+                                let msg = mqtt::Message::new(
+                                    format!("{}{}", "iotm-sys/device/logs/", get_MAC()),
+                                    "New updates downloaded.",
+                                    mqtt::QOS_1,
+                                );
+                                cli.publish(msg);
+
                                 let mut output = Command::new(
                                     "tar -xvf RPiClient-rs.tar; sudo service RPiClient-rs restart",
                                 )
                                 .output()
                                 .expect("command failed to execute");
                                 let op = String::from_utf8_lossy(&output.stdout);
-
-                                let msg = mqtt::Message::new(
-                                    format!("{}{}", "iotm-sys/device/logs/", get_MAC()),
-                                    "New updates applied.",
-                                    mqtt::QOS_1,
-                                );
-                                cli.publish(msg);
                             } else if msg.topic().contains("iotm-sys/device/client/url/all") {
                                 println!("device firmware all");
                                 let data = msg.payload_str();
