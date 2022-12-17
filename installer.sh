6574bb606d857e53db154baa42703de345ee8428
Nauman3S <naumanshakir3s@gmail.com>
2022-12-17 13:33:38 +0100

    updated installer.sh

--- installer.sh
+++ installer.sh
@@ -2,6 +2,16 @@
 # We do not want users to end up with a partially working install, so we exit the script
 # instead of continuing the installation with something broken
 set -e
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+NC='\033[0m' # No Color
 
 echo "IOT Management System Client Installer"
 
@@ -31,16 +41,15 @@ ___) (__| (___) |  | |     | )   ( |  /\____) |  | |  /\____) |
 }
 show_ascii_berry
 
-sudo pip3 install setuptools;
-sudo pip3 install vcgencmd;
-sudo pip3 install paho-mqtt;
 sudo apt install neofetch -y --force-yes;
-
+printf "${Green} Installing RPiClient-rs ${NC}\n"
 if [ -d "$HOME/RPiClient" ]
 then
-    echo "Directory RPiClient exists."
+    printf "${White} Directory RPiClient-rs already exists. ${NC}\n"
 else
-    echo "Error: Directory RPiClient does not exists."
+    
+    printf "${Red} Error: Directory RPiClient does not exists. Creating one. ${NC}\n"
+    printf "${Purple} Cloning IoTManagementSystem Repository ${NC}\n"
     cd $HOME
     # mkdir ~/RPiClient
     git clone \
@@ -49,35 +58,29 @@ else
     --sparse \
     https://github.com/Nauman3S/IOTManagementSystem;
     cd IOTManagementSystem
-    git sparse-checkout set RPiClient
-    mv RPiClient ../
+    git sparse-checkout set RPiClient-rs
+    mv RPiClient-rs ../
     cd ..
     rm -rf IOTManagementSystem
-    cd RPiClient
-    sudo chmod a+rx starter.sh
-    sudo chmod a+rx upgradeOS.sh
-    sudo chmod a+rx main.py
-    sudo chmod a+rx fw.py
-    
+    cd RPiClient-rs
+    printf "${Green} Installing systemd deamon for RPiClient-rs and RPiClient-rs-user-script ${NC}\n"
+    sudo cp RPiClient-rs.service /lib/systemd/system/
+    sudo cp RPiClient-rs-user-script.service /lib/systemd/system/
+    systemctl daemon-reload
+    sudo systemctl enable RPiClient-rs
+    sudo systemctl enable RPiClient-rs-user-script
+    sudo service RPiClient-rs start; sudo service RPiClient-rs-user-script start
 fi
 if [ -d "$HOME/RPiClient/logs" ]
 then
-    echo "Directory RPiClient/logs exists."
+    
+    printf "${White} Directory RPiClient-rs/logs already exists. ${NC}\n"
 else
-    echo "Error: Directory RPiClient does not exists."
+    printf "${Red} Error: Directory RPiClient/logs does not exists. Creating one. ${NC}\n"
     mkdir ~/RPiClient/logs
 fi
 
-File="/etc/rc.local"
-
-if [[ $(grep "(sleep 5; sh /home/pi/RPiClient/starter.sh)&" $File) ]] ; then
-    echo "Found startup script. Doing nothing."
-else
-    echo "Not Found. Adding startup script"
-    sed -i -e '$i \(sleep 5; sh /home/pi/RPiClient/starter.sh)&\n' /etc/rc.local
-fi
 
 MAC=`ip link show wlan0 | grep link/ether | awk '{print $2}' | sed 's/://g'`
 
-
-echo "Installtion Completed. Add a new device to IOTMSys with Mac Address $MAC and restart your Raspberry Pi"
+printf "${Green} Installtion Completed. Add a new device to IOTMSys with Mac Address $MAC and restart your Raspberry Pi${NC}\n"
