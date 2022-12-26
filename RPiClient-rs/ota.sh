f733db5df43bf4150b11669d8dd450c6cdf1860b
Nauman3S <naumanshakir3s@gmail.com>
2022-12-25 16:39:54 -0800

    updated OTA Script

--- RPiClient-rs/ota.sh
+++ RPiClient-rs/ota.sh
@@ -4,10 +4,10 @@ until [ -f /home/pi/RPiClient-rs/RPiClient-rs.tar ]
 do
      sleep 5
 done
-echo "File found"
+echo "OTA File Found"
 sudo tar -xvf /home/pi/RPiClient-rs/RPiClient-rs.tar
 sudo rm -rf /home/pi/RPiClient-rs/RPiClient-rs.tar
 sudo service RPiClient-rs restart
 sudo echo "Last OTA Performed at: \n `date`">/home/pi/RPiClient-rs/logs/ota.log
-echo "tim"
+echo "OTA Performed"
 exit
